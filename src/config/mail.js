67bfd1da3070905b05fe2357405286fe31d11785
nadiapd <nadiashlaf@gmail.com>
2026-05-15 11:17:11 +0700

    fix smtp config mail

--- src/config/mail.js
+++ src/config/mail.js
@@ -24,6 +24,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    // Membantu melewati kendala handshaking di lingkungan container
+    rejectUnauthorized: false 
   }
 })
 
